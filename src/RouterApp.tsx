52b761e247036fe318a50d283c1055ceb520cb7d
washablestew <washablestew@gmail.com>
2026-07-17 22:32:25 +0300

    Build Durak project page

--- src/RouterApp.tsx
+++ src/RouterApp.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import { HomePage } from './App'
 import { AvitoPage } from './pages/AvitoPage'
 import { ChillOutPage } from './pages/ChillOutPage'
+import { DurakPage } from './pages/DurakPage'
 import { GreenfieldPage } from './pages/GreenfieldPage'
 import { GazpromPage } from './pages/GazpromPage'
 import { JaecooPage } from './pages/JaecooPage'
@@ -25,6 +26,7 @@ export function PortfolioApp() {
         <Route path="/" element={<HomePage />} />
         <Route path="/projects/avito" element={<AvitoPage />} />
         <Route path="/projects/chillout" element={<ChillOutPage />} />
+        <Route path="/projects/durak" element={<DurakPage />} />
         <Route path="/projects/greenfield" element={<GreenfieldPage />} />
         <Route path="/projects/gazprom" element={<GazpromPage />} />
         <Route path="/projects/jaecoo" element={<JaecooPage />} />
